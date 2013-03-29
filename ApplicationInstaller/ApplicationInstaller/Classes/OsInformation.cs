58d912108dfb6ef65d25640193dae7bc29720bae
Mike Jarrett <blyenth@gmail.com>
2013-03-29 11:54:16 +0000

    OsInformation and slugifier
    Get Os info such as name and service pack (as well as version)
    Slugifier to take the returned os info (used to open the correct updates
    xml)

---
+++ ApplicationInstaller/ApplicationInstaller/Classes/OsInformation.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationInstaller.Classes
+{
+    static class OsInformation
+    {
+        public static int getOSArchitecture()
+        {
+            String environmentVariable = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE");
+            return ((String.IsNullOrEmpty(environmentVariable) || (String.Compare(environmentVariable, 0, "x86", 0, 3, true) == 0)) ? 0x20 : 0x40);
+        }
+
+        private static String OsString(OperatingSystem oSVersion, String versionString)
+        {
+            if (!(versionString != ""))
+            {
+                return versionString;
+            }
+            versionString = "Windows " + versionString;
+            if (oSVersion.ServicePack != "")
+            {
+                versionString = versionString + " " + oSVersion.ServicePack;
+            }
+            return (versionString + " " + getOSArchitecture().ToString() + "-bit");
+        }
+
+        public static String getOSName()
+        {
+            OperatingSystem oSVersion = Environment.OSVersion;
+            Version version = oSVersion.Version;
+            String versionString = "";
+            if (oSVersion.Platform == PlatformID.Win32Windows)
+            {
+                switch (version.Minor)
+                {
+                    case 0:
+                        versionString = "95";
+                        break;
+                    case 10:
+                        if (version.Revision.ToString() == "2222A")
+                        {
+                            versionString = "98SE";
+                        }
+                        else
+                        {
+                            versionString = "98";
+                        }
+                        break;
+                    case 90:
+                        versionString = "Me";
+                        break;
+                }
+            }
+            else if (oSVersion.Platform == PlatformID.Win32NT)
+            {
+                switch (version.Major)
+                {
+                    case 3:
+                        versionString = "NT 3.51";
+                        break;
+
+                    case 4:
+                        versionString = "NT 4.0";
+                        break;
+
+                    case 5:
+                        if (version.Minor != 0)
+                        {
+                            versionString = "XP";
+                        }
+                        else
+                        {
+                            versionString = "2000";
+                        }
+                        break;
+
+                    case 6:
+                        if (version.Minor != 0)
+                        {
+                            versionString = "7";
+                        }
+                        else
+                        {
+                            versionString = "Vista";
+                        }
+                        break;
+                }
+            }
+            return OsString(oSVersion, versionString);
+        }
+    }
+}
+
---
+++ ApplicationInstaller/ApplicationInstaller/Classes/Slugifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ApplicationInstaller.Classes
+{
+    static class Slugifier
+    {
+        // Slight modified from https://gist.github.com/onebeatconsumer/1329568
+        /// <summary>
+        /// Generates a permalink slug for passed string
+        /// </summary>
+        /// <param name="phrase"></param>
+        /// <returns>clean slug string (ex. "some-cool-topic")</returns>
+        public static string GenerateSlug(String str)
+        {
+            str = str.ToLower();
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");                      // remove invalid characters
+            str = Regex.Replace(str, @"\s+", " ").Trim();                       // single space
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();  // cut and trim
+            str = Regex.Replace(str, @"\s", "-");                               // insert hyphens
+            return str.ToLower();
+        }
+    }
+}
